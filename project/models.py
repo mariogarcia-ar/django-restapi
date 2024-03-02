879be518f728587ec7c93816745c7c865282e6b2
Mario Garcia <mariogarcia.ar@gmail.com>
2024-03-02 20:26:47 -0300

    add models to admin

--- project/models.py
+++ project/models.py
@@ -4,5 +4,8 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    tecnology = models.CharField(max_length=250)
+    technology = models.CharField(max_length=250)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id) + ' - ' + self.title
